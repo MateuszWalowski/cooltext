50c3a401e524a3f66114f560d3be444ead404b8c
Mateusz_Walowski <mat.walowski@gmail.com>
2021-03-09 01:43:04 +0100

    almost done

---
+++ script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var textarray = document.querySelector("#cooltext").textContent.split("");
+document.querySelector("#cooltext").innerHTML = "";
+var lettershown = [];
+
+textarray.forEach((letter) => {
+  let letterspan = `<span>${letter} </span>`;
+  console.log(letterspan);
+  lettershown.push(letterspan);
+});
+console.log(lettershown);
+display();
+
+function display() {
+  let nocommas = lettershown.join("");
+  document.querySelector("#cooltext").innerHTML = nocommas;
+  addanimation();
+}
+
+function addanimation() {
+  let singleletters = document.querySelectorAll("#cooltext span");
+  console.log(singleletters);
+  singleletters.forEach((letter, N) => {
+    if (letter.textContent !== "  ") {
+      setInterval(() => {
+        letter.style.display = "inline-block";
+        letter.classList.add("animation");
+      }, N * 100);
+    } else {
+      letter.textContent = " ";
+      letter.style.display = "inline-block";
+    }
+    console.log(letter.textContent);
+  });
+}
